4a1014de83915a914b28731c1071e60149ef166d
Andreu <256410991+dadifroggiebcn@users.noreply.github.com>
2026-04-03 18:15:31 +0200

    Resonable margins by defaultWq

--- src/settings.h
+++ src/settings.h
@@ -1,4 +1,4 @@
 #define COLS 25 		// Total colums for the grid
 #define ROWS 50			// Same but with rows
-#define LEFTMRG 2 		// Left margin to start placing cells from files
-#define TOPMRG 1		// Same but top margin
+#define LEFTMRG 20 		// Left margin to start placing cells from files
+#define TOPMRG 10		// Same but top margin
